67d015f2a02e68b093eb24ba556ae89b72fefd40
tomykaira <tomykaira@gmail.com>
2012-05-20 11:16:15 +0900

    Fix syntax

--- lib/rspec/parameterized.rb
+++ lib/rspec/parameterized.rb
@@ -2,9 +2,27 @@ require "rspec/parameterized/version"
 
 module RSpec
   module Parameterized
+    module ExampleMethods
+      def ittt(desc=nil, *args, &block)
+        p desc
+        param_sets.each do |params|
+          context params.inspect do
+            [arg_names, params].transpose.each do |n, p|
+              let(n) { p }
+            end
+            it(desc, *args, &block)
+          end
+        end
+      end
+    end
+
     module ExampleGroupMethods
-      def where(&b)
-        puts "here"
+
+      attr_reader :arg_names, :param_sets
+
+      def where(*args, &b)
+        @arg_names = args
+        @param_sets = b.call
       end
     end
   end
@@ -12,6 +30,7 @@ module RSpec
   module Core
     class ExampleGroup
       extend ::RSpec::Parameterized::ExampleGroupMethods
+      include ::RSpec::Parameterized::ExampleMethods
     end
   end
 end
--- spec/parametarized_spec.rb
+++ spec/parametarized_spec.rb
@@ -3,14 +3,17 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe RSpec::Parameterized do
   describe "Sample" do
     describe "plus" do
-      it "should do additions", :with_sample => true do |a, b, answer|
-        (a + b).should == answer
+      where(:a, :b, :c) do
+        [
+          [1 , 2 , 3],
+          [5 , 8 , 13],
+          [0 , 0 , 0]
+        ]
       end
 
-      where do
-        1 | 2 | 3
-        5 | 8 | 13
-        0 | 0 | 0
+      ittt "should do additions" do
+        p a, b, answer
+        (a + b).should == answer
       end
     end
   end
