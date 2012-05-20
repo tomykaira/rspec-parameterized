eb9977fdfeaf9e778e7797322d70252760bdfba4
tomykaira <tomykaira@gmail.com>
2012-05-20 11:25:28 +0900

    moved

--- lib/rspec/parameterized.rb
+++ lib/rspec/parameterized.rb
@@ -2,27 +2,23 @@ require "rspec/parameterized/version"
 
 module RSpec
   module Parameterized
-    module ExampleMethods
-      def ittt(desc=nil, *args, &block)
-        p desc
-        param_sets.each do |params|
-          context params.inspect do
-            [arg_names, params].transpose.each do |n, p|
-              let(n) { p }
-            end
-            it(desc, *args, &block)
-          end
-        end
-      end
-    end
-
     module ExampleGroupMethods
-
-      attr_reader :arg_names, :param_sets
-
       def where(*args, &b)
         @arg_names = args
         @param_sets = b.call
+      end
+
+      def with_them(*args, &block)
+        arg_names = @arg_names
+        @param_sets.each do |params|
+          describe(params.inspect, *args) do
+            [arg_names, params].transpose.each do |n|
+              let(n[0]) { n[1] }
+            end
+
+            module_eval(&block)
+          end
+        end
       end
     end
   end
@@ -30,7 +26,6 @@ module RSpec
   module Core
     class ExampleGroup
       extend ::RSpec::Parameterized::ExampleGroupMethods
-      include ::RSpec::Parameterized::ExampleMethods
     end
   end
 end
--- spec/parametarized_spec.rb
+++ spec/parametarized_spec.rb
@@ -1,9 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+# RSpec::Parameterized
+#   Sample
+#     plus
+#       [1, 2, 3]
+#         should do additions
+#       [5, 8, 13]
+#         should do additions
+#       [0, 0, 0]
+#         should do additions
+
 describe RSpec::Parameterized do
   describe "Sample" do
     describe "plus" do
-      where(:a, :b, :c) do
+      where(:a, :b, :answer) do
         [
           [1 , 2 , 3],
           [5 , 8 , 13],
@@ -11,9 +21,10 @@ describe RSpec::Parameterized do
         ]
       end
 
-      ittt "should do additions" do
-        p a, b, answer
-        (a + b).should == answer
+      with_them do
+        it "should do additions" do
+          (a + b).should == answer
+        end
       end
     end
   end
