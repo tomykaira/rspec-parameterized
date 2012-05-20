39189022d7478898d708514ae73381bd9f3ff606
tomykaira <tomykaira@gmail.com>
2012-05-20 10:53:12 +0900

    Add where to example group methods

--- lib/rspec/parameterized.rb
+++ lib/rspec/parameterized.rb
@@ -2,5 +2,16 @@ require "rspec/parameterized/version"
 
 module RSpec
   module Parameterized
+    module ExampleGroupMethods
+      def where(&b)
+        puts "here"
+      end
+    end
+  end
+
+  module Core
+    class ExampleGroup
+      extend ::RSpec::Parameterized::ExampleGroupMethods
+    end
   end
 end
