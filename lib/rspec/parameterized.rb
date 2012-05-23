8e094b8ac7874b3ee7524d64ab34824193cf91e7
joker1007 <kakyoin.hierophant@gmail.com>
2012-05-23 15:40:39 +0900

    fix params.inspect for document format. use Hash.
    In case document format,
    Hash inspection is easier to see name & value combination.
    
    and, In case I give Proc object to parameters,
    sourcify Proc object for readability

--- lib/rspec/parameterized.rb
+++ lib/rspec/parameterized.rb
@@ -92,7 +92,8 @@ module RSpec
 
       def define_cases(arg_names, param_sets, *args, &block)
         param_sets.each do |params|
-          describe(params.inspect, *args) do
+          pretty_params = [arg_names, params].transpose.inject({}) {|h, t| h.merge!(t[0] => inspect_proc(t[1]))}
+          describe(pretty_params.inspect, *args) do
             [arg_names, params].transpose.each do |n|
               let(n[0]) { n[1] }
             end
@@ -100,6 +101,10 @@ module RSpec
             module_eval(&block)
           end
         end
+      end
+
+      def inspect_proc(obj)
+        obj.class == Proc ? obj.to_source : obj
       end
     end
   end
